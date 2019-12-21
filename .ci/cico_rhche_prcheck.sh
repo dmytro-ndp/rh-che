66df5bde6c1a20d328a1041c84e5f716e764c349
Dmytro Nochevnov <dnochevn@redhat.com>
2019-12-21 12:10:49 +0200

    Turn on firewall
    Signed-off-by: Dmytro Nochevnov <dnochevn@redhat.com>

--- .ci/cico_rhche_prcheck.sh
+++ .ci/cico_rhche_prcheck.sh
@@ -41,15 +41,15 @@ nmcli > nmclioutput
 cat nmclioutput
 
 
-# firewall-cmd --permanent --new-zone dockerc
-# firewall-cmd --permanent --zone dockerc --add-source 172.17.0.0/16
-# firewall-cmd --permanent --zone dockerc --add-port 8443/tcp
-# firewall-cmd --permanent --zone dockerc --add-port 53/udp
-# firewall-cmd --permanent --zone dockerc --add-port 8053/udp
-# firewall-cmd --reload
+firewall-cmd --permanent --new-zone dockerc
+firewall-cmd --permanent --zone dockerc --add-source 172.17.0.0/16
+firewall-cmd --permanent --zone dockerc --add-port 8443/tcp
+firewall-cmd --permanent --zone dockerc --add-port 53/udp
+firewall-cmd --permanent --zone dockerc --add-port 8053/udp
+firewall-cmd --reload
 
 
-systemctl stop firewalld
+# systemctl stop firewalld
 setenforce 0
 
 
